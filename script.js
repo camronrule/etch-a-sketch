72c5a8053ce4a531c6e7c80a1858554fed2ec271
Camron Rule <camronrule@gmail.com>
2023-09-17 14:01:02 -0400

    Refactor code to fix bug
    Fixed: grid not displaying on page load
    Fixed: slider not updating

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 const GRID_SIZE_PX = 500;
+let gridSize;
+let slider;
+let gridSizeShown;
 
 //wait until page has loaded to set up listeners
 document.addEventListener('DOMContentLoaded', init, false);
 
-//add listeners for:
-//gridSize slider => create grid using gridSize => listeners for grid
-//erase button
+//called only on page load
+//responsibilities:
+//set gridSize to the number shown/the default (32x32)
+//draw a grid with that size
+//call main
 function init(){
-    let slider = document.querySelector('#slider'); //gridSize slider
-    slider.addEventListener('mouseup', function() {
-        let gridSize = this.value;
-        let gridSizeShown = document.querySelector('.rangeSlider, .value');
-        gridSizeShown.textContent = gridSize + ' x ' + gridSize;
-        clearGrid();
-        createNewGrid(gridSize);
-        addDivListeners();
-    });
+    slider = document.querySelector('#slider');
+    gridSize = slider.value; //get default value for rangeSlider
+    gridSizeShown = document.querySelector('.rangeSlider, .value');
+    gridSizeShown.textContent = gridSize + 'x' + gridSize; //set display value for gridSize to be the default of the range slider
+    slider.addEventListener('mouseup', main);
+    main();
 }
+
+//called during mouseup event on slider
+//create mouseup event listeners on slider
+function main(){
+    gridSize = slider.value;
+    gridSizeShown.textContent = gridSize + ' x ' + gridSize;
+    clearGrid();
+    createNewGrid();
+    addDivListeners();
+}
+
 
 function clearGrid(){
     let divs = document.querySelectorAll('.container.sketchboxes > div');
@@ -25,7 +38,7 @@ function clearGrid(){
 
 
 //initially size of grid is 32x32 - range from 1 to 64
-function createNewGrid(gridSize) {
+function createNewGrid() {
     let dimensions = (GRID_SIZE_PX/gridSize).toString() + 'px';
 
     for (let i = 0; i < gridSize; i++){
