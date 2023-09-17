a4f95b28147a0316f03909ebfb88097411583505
Camron Rule <camronrule@gmail.com>
2023-09-17 17:06:51 -0400

    Add color picker functionality

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-const GRID_SIZE_PX = 500;
-let gridSize;
-let slider;
+const GRID_SIZE_PX = 500; //dimensions of the sketchbox, 500x500
+let gridSize; //number of boxes in each dimension, 1x1 to 64x64
+let slider; 
 let gridSizeShown;
+let divColor = '#000000' //default - black
 
 //wait until page has loaded to set up listeners
 document.addEventListener('DOMContentLoaded', init, false);
@@ -16,6 +17,17 @@ function init(){
     //when erase button is selected, regenerate grid like new
     let erase = document.querySelector('#eraseButton');
     erase.addEventListener('click', main);
+
+    //when color input is selected, change the color the divs are filled with
+    let color = document.querySelector('#colorpicker');
+    console.log(color.value);
+    color.addEventListener('input', (e) => {
+        divColor = e.target.value;
+    }, false);
+
+    //when 'rainbow mode' is selected, the user will 
+    //draw a random color when hovering over a box
+    //let rainbow = document.querySelector('')
 
     slider = document.querySelector('#slider');
     gridSize = slider.value; //get default value for rangeSlider
@@ -50,6 +62,7 @@ function createNewGrid() {
         for (let j=0; j< gridSize; j++){
             let div = document.createElement('div');
 
+            div.style.color = divColor;
             div.style.height = dimensions;
             div.style.width = dimensions;
             div.style.outline = '1px solid #e3e3e3';
@@ -62,6 +75,6 @@ function createNewGrid() {
 function addDivListeners(){
     let divs = document.querySelectorAll('.container.sketchboxes > div');
     divs.forEach(div => div.addEventListener('mouseover', () => {
-        div.style.backgroundColor = 'black';
+        div.style.backgroundColor = divColor;
     }))
 }
