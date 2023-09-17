7d4b6fa36b952c4802ea25f54dbbddef776d09ce
Camron Rule <camronrule@gmail.com>
2023-09-17 13:40:55 -0400

    Create hover = draw functionality

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const GRID_SIZE_PX = 500;
+
 //wait until page has loaded to set up listeners
 document.addEventListener('DOMContentLoaded', init, false);
 
@@ -10,13 +12,41 @@ function init(){
         let gridSize = this.value;
         let gridSizeShown = document.querySelector('.rangeSlider, .value');
         gridSizeShown.textContent = gridSize + ' x ' + gridSize;
-        createGrid(gridSize);
+        clearGrid();
+        createNewGrid(gridSize);
+        addDivListeners();
     });
 }
 
-//initially size of grid is 32x32 - range from 1 to 64
-function createGrid(gridSize) {
-    
-    console.log(gridSize);
+function clearGrid(){
+    let divs = document.querySelectorAll('.container.sketchboxes > div');
+    divs.forEach(div => div.parentNode.removeChild(div));
 }
 
+
+//initially size of grid is 32x32 - range from 1 to 64
+function createNewGrid(gridSize) {
+    let dimensions = (GRID_SIZE_PX/gridSize).toString() + 'px';
+
+    for (let i = 0; i < gridSize; i++){
+        for (let j=0; j< gridSize; j++){
+            let div = document.createElement('div');
+
+            div.style.height = dimensions;
+            div.style.width = dimensions;
+            div.style.outline = '1px solid #e3e3e3';
+
+            document.querySelector('.container.sketchboxes').appendChild(div);
+        }
+    }
+
+    console.log(gridSize);
+
+}
+
+function addDivListeners(){
+    let divs = document.querySelectorAll('.container.sketchboxes > div');
+    divs.forEach(div => div.addEventListener('mouseover', () => {
+        div.style.backgroundColor = 'black';
+    }))
+}
